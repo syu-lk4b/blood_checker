f86bcb9438b32282d78dc333ce34709612c1a050
steven yu <steven.yu@apple.com>
2026-03-26 15:53:54 +0800

    feat(app): wire AI tab, inject LLMService and ChatStore

--- Sources/BloodPressureCamApp.swift
+++ Sources/BloodPressureCamApp.swift
@@ -3,11 +3,15 @@ import SwiftUI
 @main
 struct BloodPressureCamApp: App {
     @StateObject private var store = MeasurementStore()
+    @StateObject private var llmService = LLMService()
+    @StateObject private var chatStore = ChatStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .environmentObject(llmService)
+                .environmentObject(chatStore)
         }
     }
 }
